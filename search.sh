71902266ac532bcbfd51f3cc25f7140c66762399
varao <varao@purdue.edu>
2017-03-26 15:47:05 -0400

    fuzzy search

--- search.sh
+++ search.sh
@@ -1,10 +1,12 @@
-xterm -e '/home/varao/.fzf/bin/fzf > /tmp/found 
+xterm -e 'find * | /home/varao/.fzf/bin/fzf > /tmp/found 
 
 f_fzf=$(</tmp/found)
 f_fzf=${f_fzf// /\\ }
 
 if [ -z "${f_fzf// }" ]; then 
   echo " "
+elif [ -d "$f_fzf" ]; then 
+  echo "gnome-terminal --working-directory=$f_fzf" > /tmp/found
 elif [ ${f_fzf: -4} == ".pdf" ]; then
   echo "evince $f_fzf" > /tmp/found
 else 
