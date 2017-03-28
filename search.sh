6ae14cd07e9c473cc6779afa1316ade8b058b45d
varao <varao@purdue.edu>
2017-03-28 17:51:56 -0400

    Search.sh

--- search.sh
+++ search.sh
@@ -6,16 +6,15 @@ function run_sel() {
   f_fzf=${f_fzf// /\\ }
 
   if [ -z "${f_fzf// }" ]; then 
-    echo "" > /tmp/found
+    spwn_cmd="" 
   elif [ -d "$f_fzf" ]; then 
-    echo "gnome-terminal --working-directory=$f_fzf" > /tmp/found
+    spwn_cmd="gnome-terminal --working-directory=$f_fzf"
   elif [ ${f_fzf: -4} == ".pdf" ]; then
-    echo "nohup zathura $f_fzf &> /dev/null &" > /tmp/found
+    spwn_cmd="nohup zathura $f_fzf &> /dev/null &"
   else 
-    echo "gnome-terminal -e \"/home/varao/git/vim/src/vim $f_fzf\"" > /tmp/found
+    spwn_cmd="gnome-terminal -e \"/home/varao/git/vim/src/vim $f_fzf\"" 
   fi
-  chmod +x /tmp/found
-  /tmp/found
+  eval $spwn_cmd
 }
 export -f run_sel
 
@@ -24,12 +23,10 @@ stdir_o=$(dirname $stdir)  # get parent dir
 f_fzf="tmp"
 
 while [ ! -z "${f_fzf// }" ]; do
-  echo $f_fzf
-  f_fzf="$({ echo $stdir_o; /home/varao/git/bfs/bfs $stdir -exclude -name .git 2> /dev/null | tail -n +2 ; } | /home/varao/.fzf/bin/fzf --bind "tab:accept,enter:execute(run_sel {} &)")"
+  f_fzf="$({ echo $stdir_o; /home/varao/git/bfs/bfs $stdir -exclude -name .git 2> /dev/null | tail -n +2 ; } | /home/varao/.fzf/bin/fzf +s --bind "tab:accept,enter:execute(run_sel {} &)")"
   stdir=$f_fzf
   stdir_o=$(dirname $stdir)
 done
 # installed bfs from https://github.com/tavianator/bfs, branch exclude
 
 '
-
