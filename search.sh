21240d5cc3ac768908147660f579266294326a7e
varao <varao@purdue.edu>
2017-03-31 23:34:11 -0400

    Dot files

--- search.sh
+++ search.sh
@@ -1,4 +1,4 @@
-xterm -name fzf_term -e '
+xterm +hm -name fzf_term -e '
 
 function run_sel() {
   f_fzf=$1
@@ -9,7 +9,7 @@ function run_sel() {
   elif [ -d "$f_fzf" ]; then 
     spwn_cmd="gnome-terminal --working-directory=$f_fzf"
   elif [ ${f_fzf: -4} == ".pdf" ]; then
-    spwn_cmd="nohup zathura $f_fzf &> /dev/null &"
+    spwn_cmd="nohup zathura-tabbed $f_fzf &> /dev/null &"
   else 
     spwn_cmd="gnome-terminal -e \"/home/varao/git/vim/src/vim $f_fzf\"" 
   fi
@@ -23,7 +23,8 @@ f_fzf="tmp"
 
 while [ ! -z "${f_fzf// }" ]; do
   echo $f_fzf
-  f_fzf="$({ echo $stdir_o; /home/varao/git/bfs/bfs "$stdir" -exclude -name .git 2> /dev/null | tail -n +2 ; } | /home/varao/.fzf/bin/fzf +s --bind "tab:accept,enter:execute(run_sel {} &)")"
+  f_fzf="$({ echo $stdir_o; 
+             /home/varao/git/bfs/bfs "$stdir" -color -exclude -name .git 2> /dev/null | tail -n +2 ; } | /home/varao/.fzf/bin/fzf +s --ansi --color=fg+:-1,spinner:5,hl:1,hl+:1,info:5,bg+:7 --bind "tab:accept,enter:execute(run_sel {} &)")"
   stdir=$f_fzf
   # Need quotes inside $() (and above) for whitespaces in filenames 
   stdir_o=$(dirname "$stdir") 
