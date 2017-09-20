f1825e2483c5c76f0eaaa0c82705cd08377fa270
varao <varao@purdue.edu>
2017-09-20 15:47:57 -0400

    xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE PackageImports, OverloadedStrings #-}
-
+{-# LANGUAGE PackageImports, OverloadedStrings, FlexibleInstances, MultiParamTypeClasses #-} 
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
@@ -18,6 +17,7 @@ import XMonad.Util.EZConfig ( additionalKeys )
 import XMonad.Util.Font
 import XMonad.Util.Scratchpad
 import XMonad.Util.NamedScratchpad
+import XMonad.Util.Loggers
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 import XMonad.Layout.SubLayouts
@@ -26,6 +26,9 @@ import XMonad.Layout.ResizableTile
 
 import XMonad.Layout.BoringWindows
 import XMonad.Layout.Grid
+
+import Data.Maybe (fromJust)
+import XMonad.Layout.LayoutModifier
 
 import qualified "dbus" DBus as D
 import qualified "dbus" DBus.Client as D
@@ -40,15 +43,45 @@ myTheme = def {
 --            decoHeight = 30,
             fontName = "xft:DejaVu Sans:size=10" }
 
-myLayout = avoidStruts  -- Makes gnome panel visible
+---------------------------------
+-- workaround of xmonad issue 4
+-- https://wiki.haskell.org/Xmonad/Config_archive/hgabreu%27s_xmonad.hs 
+data FixFocus a = FixFocus (Maybe a) deriving (Read, Show)
+instance LayoutModifier FixFocus Window where
+    modifyLayout (FixFocus mlf) ws@(W.Workspace id lay Nothing) r = runLayout ws r
+    modifyLayout (FixFocus Nothing) ws r = runLayout ws r
+    modifyLayout (FixFocus (Just lf)) (W.Workspace id lay (Just st)) r = do
+        let stack_f = W.focus st  -- get current stack's focus
+        mst <- gets (W.stack . W.workspace . W.current . windowset)
+        let mreal_f = maybe Nothing (Just . W.focus) mst -- get Maybe current real focus
+        is_rf_floating <- maybe (return False) (\rf -> withWindowSet $ return . M.member rf . W.floating) mreal_f -- real focused window is floating?
+        let new_stack_f = if is_rf_floating then lf else stack_f --if yes: replace stack's focus with our last saved focus
+        let new_st' = until (\s -> new_stack_f == W.focus s) W.focusUp' st -- new stack with focused new_stack_f
+        let new_st = if (new_stack_f `elem` (W.integrate st)) then new_st' else st -- use it only when it's possible to
+        runLayout (W.Workspace id lay (Just new_st)) r
+ 
+    redoLayout (FixFocus mlf) r Nothing wrs = return (wrs, Just $ FixFocus mlf)
+    redoLayout (FixFocus mlf) r (Just st) wrs = do
+        let stack_f = W.focus st  -- get current stack's focus
+        mst <- gets (W.stack . W.workspace . W.current . windowset)
+        let mreal_f = maybe Nothing (Just . W.focus) mst -- get Maybe current real focus
+        let crf_in_stack = maybe False ((flip elem) (W.integrate st)) mreal_f -- current real focus belongs to stack?
+        let new_saved_f = if crf_in_stack then fromJust mreal_f else stack_f -- if yes: replace saved focus
+        return (wrs, Just $ FixFocus $ Just new_saved_f)
+ 
+fixFocus :: LayoutClass l a => l a -> ModifiedLayout FixFocus l a
+fixFocus = ModifiedLayout $ FixFocus Nothing
+---------------------------------
+
+myLayout = fixFocus $ avoidStruts  -- Makes gnome panel visible
          $ windowNavigation 
          $ addTabs shrinkText myTheme -- def: default
          $ boringWindows $ tall 
               where 
                 rt = ResizableTall 1 (3/100) (1/2) []
                 tall =  subLayout [] 
-                  (Simplest ||| spiral (6/7) ||| Grid) 
-                  $  rt ||| Mirror(rt)
+                  (Simplest ||| spiral (6/7) ||| Grid) -- 
+                  $  rt 
                    -- |||spiral (6/7) ||| Mirror(rt) 
                    ||| Full
 
@@ -87,7 +120,7 @@ myKeys =
     , ((myModMask .|. controlMask, xK_Down), sendMessage $ pullGroup D)
    -- resize windows
     , ((myModMask,               xK_a), sendMessage MirrorShrink)
-    , ((myModMask,               xK_z), sendMessage MirrorExpand)
+    , ((myModMask,               xK_s), sendMessage MirrorExpand)
     --Merge/UnMerge
     , ((myModMask .|. controlMask, xK_m), withFocused (sendMessage . MergeAll))
     , ((myModMask .|. controlMask, xK_u), withFocused (sendMessage . UnMerge))
@@ -106,19 +139,36 @@ myKeys =
 --    , ((myModMask                , xK_f), spawn myFzf)
     , ((myModMask                , xK_f), scratchFzf)
     , ((myModMask                , xK_z), scratchZthr)
+    , ((myModMask                , xK_x), scratchFfx)
+    , ((myModMask .|. shiftMask  , xK_x), scratchChrm)
+    , ((myModMask                , xK_r), scratchRemm)
+--    , ((myModMask .|. shiftMask  , xK_x), scratchWmail)
+-- also gone: xK_b, xK_w, xK_e
   ]
   where
     -- this simply means "find the scratchpad in myScratchPads that is 
     -- named fuzzyfind and launch it"
     scratchFzf   = namedScratchpadAction myScratchPads "fuzzyfind"
     scratchZthr  = namedScratchpadAction myScratchPads "zathur"
+    scratchFfx   = namedScratchpadAction myScratchPads "firefox-p"
+    scratchChrm  = namedScratchpadAction myScratchPads "chromium"
+    scratchRemm  = namedScratchpadAction myScratchPads "remmina"
+--    scratchWmail  = namedScratchpadAction myScratchPads "wmail"
 
 myScratchPads = [  NS "fuzzyfind"  myFzf  findFZ  (customFloating $ W.RationalRect (1/8) (1/6) (1/3) (2/3))  -- one scratchpad
+                 , NS "firefox-p"  "firefox -private-window" findFfx nonFloating  -- one scratchpad
+                 , NS "chromium"   "chromium-browser" findChrm nonFloating  -- one scratchpad
+--                 , NS "wmail"   "~/git/INSTALL/WMail-linux-x64/WMail" findWmail nonFloating  -- one scratchpad
                  , NS "zathur"    "zathura-tabbed" findZth nonFloating  -- one scratchpad
+                 , NS "remmina"    "remmina" findRemm nonFloating  -- one scratchpad
                 ]
   where
    findFZ   = resource  =? "fzf_term"  
    findZth  = className =? "tabbed"  
+   findFfx  = className =? "Firefox"  
+   findChrm = className =? "Chromium-browser"  
+   findRemm = className =? "Remmina"  
+--   findWmail = className =? "wmail"  
 
 myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
   [
@@ -151,6 +201,7 @@ main = do
                       >> updatePointer (0.25, 0.25) (0.25, 0.25) -- near the top-left
            , layoutHook         = smartBorders $ myLayout
            , borderWidth        = myBorderWidth
+--           , focusFollowsMouse  = False
            , focusedBorderColor = myFocusedBorderColor
            , normalBorderColor  = myNormalBorderColor
            , startupHook        = myStartupHook
@@ -158,6 +209,7 @@ main = do
                                     <+> namedScratchpadManageHook myScratchPads 
                                     <+> manageHook defaultConfig -- uses default too
            , mouseBindings      = myMouseBindings
+--           , modMask = mod4Mask  -- Temp fix to make windows key the modifier if I really need Alt
          } `additionalKeys` myKeys
 
 prettyPrinter :: D.Client -> PP
@@ -170,7 +222,11 @@ prettyPrinter dbus =  namedScratchpadFilterOutWorkspacePP $ defaultPP
     , ppHidden   = pangoColor "grey70" . wrap "[" "]" . pangoSanitize
     , ppUrgent   = pangoColor "red"
     , ppSep      = " "
+--    , ppExtras   = [logCmd "date"]
+--    , ppExtras   = [pangoColor "skyblue" `onLogger` lTitle]
     }
+    where
+       --lTitle = fixedWidthL AlignRight " " 199 . shortenL 80 $ logCmd "dropbox status"
 
 getWellKnownName :: D.Client -> IO ()
 getWellKnownName dbus = do
