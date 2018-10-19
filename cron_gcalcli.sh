8d2df91142da218007949b06fb86f5be32945331
varao <varao@purdue.edu>
2018-10-19 19:54:57 -0400

    conky

--- cron_gcalcli.sh
+++ cron_gcalcli.sh
@@ -6,3 +6,13 @@ then
 else 
   rm /home/varao/.gcalcli_tmp 
 fi
+sleep 1
+
+curl -B wttr.in | sed 's/\x1b\[[0-9;]*m//g' > /home/varao/.gcalcli_tmp 
+if test $? -eq 0 
+then 
+  mv /home/varao/.gcalcli_tmp /home/varao/.wthr 
+else 
+  rm /home/varao/.gcalcli_tmp 
+fi
+sleep 1
