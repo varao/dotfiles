1ba68af95d53d573584de06dede99e4d409a995e
varao <varao@purdue.edu>
2018-11-19 11:09:29 -0500

    Cleaned

--- mystart.sh
+++ mystart.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 # Add command to execute in SystemTools/Preferences/StartupApplications
-xmodmap ~/.Xmodmap 
-synclient TapButton3=2
-xcape -e "Shift_R=Escape" #;Hyper_L=Tab"
+#xmodmap ~/.Xmodmap 
+#synclient TapButton3=2
+#xcape -e "Shift_R=Escape" #;Hyper_L=Tab"
+
+sleep 1 && conky -x 500; sleep 1 && conky -x 2500
