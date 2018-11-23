76702c9a7006b29f1c0790fd8c35eae6ba419204
varao <varao@purdue.edu>
2018-11-23 18:20:38 -0500

    Touchpad

--- mystart.sh
+++ mystart.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Add command to execute in SystemTools/Preferences/StartupApplications
-#xmodmap ~/.Xmodmap 
 #synclient TapButton3=2
+
+syndaemon -i 1 -K -d  # disable touchpad for 1 sec while typing
+
 xcape -e "Shift_R=Escape" #;Hyper_L=Tab"
 
 sleep 1 && conky -x 500; sleep 1 && conky -x 2500
+
+
