b30890f4af4756a9623c574d31a7819c6d364bc7
varao <varao@purdue.edu>
2019-07-17 17:16:03 -0400

    cron

--- cron_gcalcli.sh
+++ cron_gcalcli.sh
@@ -2,17 +2,17 @@
 /home/varao/Python/anaconda3/bin/gcalcli --conky --nocolor --lineart unicode --calendar "vinayak.apr" agenda  "7am" "next 7 days" > /home/varao/.gcalcli_tmp 
 if test $? -eq 0 
 then 
-  mv /home/varao/.gcalcli_tmp /home/varao/.gcalcli 
+  mv $HOME/.gcalcli_tmp $HOME/.gcalcli 
 else 
-  rm /home/varao/.gcalcli_tmp 
+  rm $HOME/.gcalcli_tmp 
 fi
 sleep 1
 
-curl -B wttr.in/laf | sed 's/\x1b\[[0-9;]*m//g' > /home/varao/.gcalcli_tmp 
-if test $? -eq 0 && grep -q Purdue /home/varao/.gcalcli_tmp
+curl -B wttr.in/laf | sed 's/\x1b\[[0-9;]*m//g' > $HOME/.gcalcli_tmp 
+if test $? -eq 0 && grep -q Purdue $HOME/.gcalcli_tmp
 then 
-  mv /home/varao/.gcalcli_tmp /home/varao/.wthr 
+  mv $HOME/.gcalcli_tmp $HOME/.wthr 
 else 
-  rm /home/varao/.gcalcli_tmp 
+  rm $HOME/.gcalcli_tmp 
 fi
 sleep 1
