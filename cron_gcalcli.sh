f3f28e1994bc45c4454ea74c161b157924f748f1
varao <varao@purdue.edu>
2020-05-11 20:47:22 -0400

    dotfiles

--- cron_gcalcli.sh
+++ cron_gcalcli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/home/varao/Python/anaconda3/bin/gcalcli --conky --nocolor --lineart unicode --calendar "vinayak.apr" agenda  "7am" "next 7 days" > /home/varao/.gcalcli_tmp 
+/home/varao/Python/miniconda/bin/gcalcli --conky --nocolor --lineart unicode --calendar "vinayak.apr" agenda  "7am" "next 7 days" > /home/varao/.gcalcli_tmp 
 if test $? -eq 0 
 then 
   mv $HOME/.gcalcli_tmp $HOME/.gcalcli 
